a1c1bcab4045896a08d0b9d63ba4de0a67c4f1ea
mzaki2599 <mzaki2599@gmail.com>
2025-03-14 23:18:14 +0530

    Binary search tree

--- Trees/BinarySearchTree.js
+++ Trees/BinarySearchTree.js
@@ -24,4 +24,19 @@ class BinarySearchTree {
         }
     }
 
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
 }
